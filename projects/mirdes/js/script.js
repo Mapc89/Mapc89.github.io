fef61584e6916cb5ac16e9821077c59e775e7d78
Mapc89 <mars.imus@yandex.ru>
2020-12-30 19:06:48 +0500

    Окна всплавают...частично

--- projects/mirdes/js/script.js
+++ projects/mirdes/js/script.js
@@ -3,33 +3,23 @@ window.addEventListener('DOMContentLoaded', () => {
     let content = document.querySelector('.content'),
         priceList = document.querySelector('.price-list'),
         optionServices = document.querySelector('.option-services'),
-        order = document.querySelector('.order'),
+        order = document.querySelector('button'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
 
     //Кнопки Заказать услугу
-    // order.addEventListener('click', function () {
-    //     overlay.style.display = 'block';
-    //     document.body.style.overflow = 'hidden';
-    //     header.style.right = '9px';
-    //     main.style.paddingRight = '17px';
-    //     content.style.paddingRight = '17px';
-    // });
     priceList.addEventListener('click', (event) => {
         console.log('1');
         let target = event.target;
         if (target.className == 'button-active order') {
             console.log('2');
-            for (let i = 0; i < order.length; i++) {
-                if (target == order[i]) {
-                    console.log('3');
-                    overlay.style.display = 'block';
-                    document.body.style.overflow = 'hidden';
-                    // header.style.right = '9px';
-                    // content.style.paddingRight = '17px';
-                    break;
-                }
-            }
+            console.log(order);
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+            // header.style.right = '9px';
+            // content.style.paddingRight = '17px';
+        } else {
+            console.log('0');            
         }
     });
     close.addEventListener('click', function () {
