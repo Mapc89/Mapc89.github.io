0e88e5afaa377948829d3cabf49c56790cd584fe
Mapc89 <mars.imus@yandex.ru>
2020-12-31 13:06:53 +0500

    Все окна всплывают

--- projects/mirdes/js/script.js
+++ projects/mirdes/js/script.js
@@ -1,13 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
     //Модальное окно
-    let content = document.querySelector('.content'),
-        priceList = document.querySelector('.price-list'),
+    let priceList = document.querySelector('.price-list'),
         optionServices = document.querySelector('.option-services'),
         order = document.querySelector('button'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
 
-    //Кнопки Заказать услугу
+    //Кнопки Заказать проект
     priceList.addEventListener('click', (event) => {
         console.log('1');
         let target = event.target;
@@ -16,8 +15,23 @@ window.addEventListener('DOMContentLoaded', () => {
             console.log(order);
             overlay.style.display = 'block';
             document.body.style.overflow = 'hidden';
-            // header.style.right = '9px';
-            // content.style.paddingRight = '17px';
+            priceList.style.paddingRight = '17px';
+            optionServices.style.paddingRight = '17px';
+        } else {
+            console.log('0');            
+        }
+    });
+    //Заказать услугу
+    optionServices.addEventListener('click', (event) => {
+        console.log('1');
+        let target = event.target;
+        if (target.className == 'button-active order') {
+            console.log('2');
+            console.log(order);
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+            priceList.style.paddingRight = '17px';
+            optionServices.style.paddingRight = '17px';
         } else {
             console.log('0');            
         }
@@ -25,8 +39,8 @@ window.addEventListener('DOMContentLoaded', () => {
     close.addEventListener('click', function () {
         overlay.style.display = 'none';
         document.body.style.overflow = '';
-        // header.style.right = '';
-        // content.style.paddingRight = '';
+        priceList.style.paddingRight = '';
+        optionServices.style.paddingRight = '';
     });
     
 });
