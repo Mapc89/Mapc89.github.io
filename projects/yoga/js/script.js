94c10c76fcfaae5b04ca887022ad3418a4b23533
Mapc89 <mars.imus@yandex.ru>
2021-06-02 11:12:32 +0500

    Продлил таймер Йоги

--- projects/yoga/js/script.js
+++ projects/yoga/js/script.js
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //Таймер
 
-    let deadline = '2020-12-31';
+    let deadline = '2021-12-31';
 
 
     function getTimeRemining(endtime) {
