2a7f5ff99593168e08ae0662d6eaeafcd913f530
Mapc89 <34654965+Mapc89@users.noreply.github.com>
2022-02-13 14:34:08 +0500

    Раскрывающиеся проекты
    Теперь переименовывается кнопка

--- projects/mirdes/js/script.js
+++ projects/mirdes/js/script.js
@@ -2,16 +2,17 @@ window.addEventListener('DOMContentLoaded', () => {
     // Раскрыие галереи
     $(function(){
         $('.btnm').click(function(){
-            let id = this.id;
-            console.log(id);
-            let test = document.getElementsByClassName(id);
-            console.log(test);
+            let id = this.id; // Берём ID кнопки
+            let test = document.getElementsByClassName(id); //Находим more с классом идентичным ID этой кнопики
             $(test).slideToggle(1000); 
+            $(this).html($(this).html() == 'скрыть' ? 'смотреть проект' : 'скрыть'); // меняет надпись в кнопке
             $(test)[0].scrollIntoView({
             behavior: "smooth", // or "auto" or "instant"
             });
+
+            
             // $('.more').slideToggle(1000); 
-            // $('.btnm').html($('.btnm').html() == 'скрыть' ? 'смотреть ещё' : 'скрыть'); // меняет надпись в кнопке
+            // $('.btnm').html($('.btnm').html() == 'скрыть' ? 'смотреть проект' : 'скрыть'); // меняет надпись в кнопке
             // $(".more")[0].scrollIntoView({
             //     behavior: "smooth", // or "auto" or "instant"
             // });
